d1e53e39f9b765bf2bf4c30300dffe09171505b0
jarabadaniela <jarabadaniela97@gmail.com>
2026-06-19 16:23:02 -0500

    Insertar biblioteca pydantic

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPIHTTPException
+from pydantic import BaseModel
 mi_app= FastAPI ()
 
 @mi_app.get ("/proyecto")
 
 def mensaje ():
-    return{"proyecto": "este es el proyecto de clientes a desarrollar"}
+    return{"proyecto": "este es el proyecto de clientes a desarrollar.."}
 
 
 @mi_app.get ("/clientes")
 def clientes ():
     mi_lista= ["zuleima","yober","quiroga","sunsi","kamila","mady"]
     return {"clientes": mi_lista}
+
+
+
+
