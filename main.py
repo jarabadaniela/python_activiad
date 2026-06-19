d887793d27288eb83a9c1b49e71f2b97b024dd4d
jarabadaniela <jarabadaniela97@gmail.com>
2026-06-19 16:27:52 -0500

    Se agrego clase cliente, factura, transacción

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPIHTTPException
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 mi_app= FastAPI ()
 
@@ -13,6 +13,21 @@ def clientes ():
     mi_lista= ["zuleima","yober","quiroga","sunsi","kamila","mady"]
     return {"clientes": mi_lista}
 
+class Cliente(BaseModel):
+    id: int
+    nombre: str
+    email: str
+    descripcion: str
 
 
+class Factura(BaseModel):
+    id: int
+    cliente_id: int
+    vrtotal: float
 
+
+class Transaccion(BaseModel):
+    id: int
+    vr_unitario: float
+    cantidad: int
+    factura_id: int
